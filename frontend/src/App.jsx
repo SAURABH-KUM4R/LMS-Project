5e592d4bbad8a80eda03701d42d74e4b249eb236
SAURABH-KUM4R <saurabhkumar65535@gmail.com>
2023-09-21 16:04:26 +0530

    HomePage Configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
-import { Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './Component/Footer'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/Homepage";
 
 function App() {
-
   return (
     <>
-      {/* <Routes>
-        
-      </Routes> */}
-      {/* <Footer/> */}
+      <Routes>
+        <Route path="/" element={<HomePage/>} ></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
